49eea715d5fa4691856573cd836433844fd11443
Jari Jokinen <jari@j2digital.io>
2024-11-23 20:14:18 +0200

    Add curses

--- src/twsbot/cli.py
+++ src/twsbot/cli.py
@@ -1,5 +1,21 @@
 import argparse
+import curses
+
 from . import __version__
+
+def curses_main(stdscr):
+    curses.curs_set(0)
+    stdscr.nodelay(1)
+    
+    stdscr.clear()
+    stdscr.addstr(0, 0, f'twsbot {__version__}', curses.A_REVERSE)
+    
+    while True:
+        stdscr.refresh()
+
+        key = stdscr.getch()
+        if key == ord('q'):
+            break
 
 def main():
     parser = argparse.ArgumentParser(
@@ -13,3 +29,5 @@ def main():
         version=f'%(prog)s {__version__}'
     )
     args = parser.parse_args()
+
+    curses.wrapper(curses_main)
